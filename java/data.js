bd291c6777f6e4f50c1a47b4896adebdc62d6f34
MigueChechoooo <127440243+MiguelChechooo@users.noreply.github.com>
2023-11-23 11:02:25 -0500

    Add files via upload

---
+++ java/data.js
@@ -0,0 +1,41 @@
+export  const vehiculos = 
+    [
+    {
+        id:"tin1",
+        nombre:"busetas",
+        descripcion:"capacidad maxima 30 personas,pero tiren pa atras que cabe otro ",
+        imagen:"../img/busetas.png",
+    },
+    {
+        id:"tin2",
+        nombre:"eclipse",
+        descripcion:"mitsubishi eclipse modelo 1234",
+        imagen:"../img/eclise.png",
+    },
+    {
+        id:"tin3",
+        nombre:"mazda 3 ",
+        descripcion:"mazda 3 con capacidad maxima de 18 personas",
+        imagen:"../img/m3.png",
+    },
+    {
+        id:"tin4",
+        nombre:"rx7",
+        descripcion:"corvette rx7",
+        imagen:"../img/rx7.png",
+    },
+    {
+        id:"tin5",
+        nombre:"silvia 15",
+        descripcion:"nissan s15",
+        imagen:"../img/s15.png",
+    },
+    {
+        id:"tin6",
+        nombre:"subaru",
+        descripcion:"inspirado en el wrx rally",
+        imagen:"../img/subaru.png",
+    },
+
+    
+    ]
